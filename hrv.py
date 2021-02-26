48debf06bdc6f0a9b48fbf322e6fb2c738ce08f5
Paula Alvarez <paula@localhost.localdomain>
2021-02-26 13:00:14 -0500

    new widget de metricas

--- hrv.py
+++ hrv.py
@@ -51,7 +51,7 @@ class Window(QMainWindow):
         self.ui.showMaximized()
 
         ## ESTE ES EL BOTON DEL DIALOGO ##
-        self.pushButtonExport.clicked.connect(self.mostrarDialogo)
+        self.metadata.clicked.connect(self.mostrarDialogo)
 
     def mostrarDialogo(self):
         dialog = Dialog(self)  # self hace referencia al padre
